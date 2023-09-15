17ee6e42559bc131c57638a86a04e72e03375183
Juannie-PP <114276909+Juannie-PP@users.noreply.github.com>
2023-09-16 01:20:54 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="tenon",
-    version="0.1.1",
+    version="0.1.2",
     package_dir={"": "src"},
     packages=["tenon"],
     url="https://github.com/Juannie-PP/tenon",
