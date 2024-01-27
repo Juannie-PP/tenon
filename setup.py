c525e206cc453a2f54d1ce487210f733d3773ae5
Juannie-PP <2604868278@qq.com>
2024-01-27 16:13:20 +0800

    新增像素点标准差和范围设置,可调整值去除裁切图片时的噪点

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="tenon",
-    version="0.1.5",
+    version="0.1.6",
     package_dir={"": "src"},
     packages=["tenon"],
     url="https://github.com/Juannie-PP/tenon",
